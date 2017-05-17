a4d7fc0895224fcb10f540eba95e8b8c8e29de25
zhs007 <sssxueren@gmail.com>
2017-05-17 16:33:54 +0800

    更新例子

--- bin/jrj_fund.js
+++ bin/jrj_fund.js
@@ -5,6 +5,8 @@ let util = require('util');
 let fs = require('fs');
 let mysql = require('mysql2/promise');
 
+const MODE_INITBASE = false;
+
 const mysqlcfg = JSON.parse(fs.readFileSync('./mysqlcfg_hfdb.json').toString());
 
 class FundState {
@@ -49,11 +51,136 @@ class FundMgr {
             await this.conn.query(str);
         }
     }
+
+    async saveFundArch(fundcode, lst, year) {
+        for (let mi = 1; mi <= 12; ++mi) {
+            let curm = '' + year + '-';
+            if (mi < 10) {
+                curm += '0';
+            }
+
+            curm += mi;
+
+            for (let di = 1; di <= 31; ++di) {
+                let curday = curm + '-';
+                if (di < 10) {
+                    curday += '0';
+                }
+
+                curday += di;
+
+                if (lst.hasOwnProperty(curday)) {
+                    let curarch = lst[curday];
+
+                    let str = util.format("insert into networth(fundcode, enddate, accum_net, unit_net, unit_net_chng_1, unit_net_chng_pct, unit_net_chng_pct_1_mon, unit_net_chng_pct_1_week, unit_net_chng_pct_1_year, unit_net_chng_pct_3_mon, guess_net) " +
+                        "values('%s', '%s', %d, %d, %d, %d, %d, %d, %d, %d, %d);",
+                        fundcode, curday, curarch.accum_net, curarch.unit_net, curarch.unit_net_chng_1,
+                        curarch.unit_net_chng_pct, curarch.unit_net_chng_pct_1_mon, curarch.unit_net_chng_pct_1_week,
+                        curarch.unit_net_chng_pct_1_year, curarch.unit_net_chng_pct_3_mon, curarch.guess_net);
+
+                    //console.log(str);
+                    await this.conn.query(str);
+                }
+            }
+        }
+    }
 };
 
 FundMgr.singleton = new FundMgr();
 
-// 主页面配置
+// fund净值页面配置
+let fundarchOptions = {
+    fundcode: '000975',
+    year: 2016,
+
+    // 主地址
+    uri: 'http://fund.jrj.com.cn/json/archives/history/netvalue?fundCode=000975&obj=obj&date=2016',
+    timeout: 1500,
+    force_encoding: 'gbk',
+
+    // 爬虫类型
+    crawler_type: CRAWLER.REQUEST,
+
+    // 数据分析配置
+    dataanalysis_type: DATAANALYSIS.NULL,
+
+    // 持久化配置
+    storage_type: STORAGE.SQL,
+    storage_cfg: {
+        filename: 'fund.sql',
+        func_procline: ld => {
+            return util.format("insert into fundbase(name, code) values('%s', '%s');", ld.name, ld.fundcode);
+        }},
+
+    // 分析数据
+    func_analysis: async crawler => {
+        let fundinfo = FundMgr.singleton.map[crawler.options.fundcode];
+
+        let jsonstr = crawler.data.substr(8);
+        let obj = JSON.parse(jsonstr);
+
+        if (obj.hasOwnProperty('fundHistoryNetValue') && Array.isArray(obj.fundHistoryNetValue)) {
+            for (let ii = 0 ; ii < obj.fundHistoryNetValue.length; ++ii) {
+                let curfdnv = obj.fundHistoryNetValue[ii];
+
+                fundinfo.lstval[curfdnv.enddate] = {
+                    enddate: curfdnv.enddate,
+                    accum_net: Math.floor(parseFloat(curfdnv.accum_net) * 10000),
+                    unit_net: Math.floor(parseFloat(curfdnv.unit_net) * 10000),
+                    unit_net_chng_1: Math.floor(parseFloat(curfdnv.unit_net_chng_1) * 10000),
+                    unit_net_chng_pct: Math.floor(parseFloat(curfdnv.unit_net_chng_pct) * 10000),
+                    unit_net_chng_pct_1_mon: Math.floor(parseFloat(curfdnv.unit_net_chng_pct_1_mon) * 10000),
+                    unit_net_chng_pct_1_week: Math.floor(parseFloat(curfdnv.unit_net_chng_pct_1_week) * 10000),
+                    unit_net_chng_pct_1_year: Math.floor(parseFloat(curfdnv.unit_net_chng_pct_1_year) * 10000),
+                    unit_net_chng_pct_3_mon: Math.floor(parseFloat(curfdnv.unit_net_chng_pct_3_mon) * 10000),
+                    guess_net: 0
+                };
+
+                if (isNaN(fundinfo.lstval[curfdnv.enddate].unit_net)) {
+                    fundinfo.lstval[curfdnv.enddate].unit_net = 0;
+                }
+
+                if (isNaN(fundinfo.lstval[curfdnv.enddate].accum_net)) {
+                    fundinfo.lstval[curfdnv.enddate].accum_net = 0;
+                }
+
+                if (isNaN(fundinfo.lstval[curfdnv.enddate].unit_net_chng_1)) {
+                    fundinfo.lstval[curfdnv.enddate].unit_net_chng_1 = 0;
+                }
+
+                if (isNaN(fundinfo.lstval[curfdnv.enddate].unit_net_chng_pct)) {
+                    fundinfo.lstval[curfdnv.enddate].unit_net_chng_pct = 0;
+                }
+
+                if (isNaN(fundinfo.lstval[curfdnv.enddate].unit_net_chng_pct_1_mon)) {
+                    fundinfo.lstval[curfdnv.enddate].unit_net_chng_pct_1_mon = 0;
+                }
+
+                if (isNaN(fundinfo.lstval[curfdnv.enddate].unit_net_chng_pct_1_week)) {
+                    fundinfo.lstval[curfdnv.enddate].unit_net_chng_pct_1_week = 0;
+                }
+
+                if (isNaN(fundinfo.lstval[curfdnv.enddate].unit_net_chng_pct_1_year)) {
+                    fundinfo.lstval[curfdnv.enddate].unit_net_chng_pct_1_year = 0;
+                }
+
+                if (isNaN(fundinfo.lstval[curfdnv.enddate].unit_net_chng_pct_3_mon)) {
+                    fundinfo.lstval[curfdnv.enddate].unit_net_chng_pct_3_mon = 0;
+                }
+            }
+
+            if (crawler.options.year == 2017) {
+                for (let iy = 2015; iy <= 2017; ++iy) {
+                    await FundMgr.singleton.saveFundArch(crawler.options.fundcode, fundinfo.lstval, iy);
+                }
+            }
+        }
+
+        return crawler;
+    }
+};
+
+// fund主页面配置
 let fundbaseOptions = {
     // 主地址
     uri: '',
@@ -138,11 +265,28 @@ let totalfundOptions = {
                 let str0 = element.attribs.href.split(',');
                 let fundcode = str0[1].split('.')[0];
 
-                FundMgr.singleton.addFund({name: element.attribs.title, url: element.attribs.href, fundcode: fundcode, fsarr: []});
+                FundMgr.singleton.addFund({
+                    name: element.attribs.title,
+                    url: element.attribs.href,
+                    fundcode: fundcode,
+                    fsarr: [],
+                    lstval: {}
+                });
 
-                let co = Object.assign({}, fundbaseOptions);
-                co.uri = element.attribs.href;
-                CrawlerMgr.singleton.addCrawler(co);
+                if (MODE_INITBASE) {
+                    let co = Object.assign({}, fundbaseOptions);
+                    co.uri = element.attribs.href;
+                    CrawlerMgr.singleton.addCrawler(co);
+                }
+                else {
+                    for (let year = 2015; year <= 2017; ++year) {
+                        let co = Object.assign({}, fundarchOptions);
+                        co.fundcode = fundcode;
+                        co.year = year;
+                        co.uri = util.format('http://fund.jrj.com.cn/json/archives/history/netvalue?fundCode=%s&obj=obj&date=%d', fundcode, year);
+                        CrawlerMgr.singleton.addCrawler(co);
+                    }
+                }
 
                 // return false;
                 //crawler.storage.pushData({name: element.attribs.title, url: element.attribs.href, fundcode: fundcode});
@@ -163,6 +307,7 @@ CrawlerMgr.singleton.processDelayTime = 0.3;
 
 FundMgr.singleton.init(mysqlcfg).then(() => {
     CrawlerMgr.singleton.addCrawler(totalfundOptions);
+    //CrawlerMgr.singleton.addCrawler(fundarchOptions);
     CrawlerMgr.singleton.start(true, true, async () => {
         FundState.singleton.output();
 
