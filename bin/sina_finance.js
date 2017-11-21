d8a01a4d7722f706c1bcede0a97fd2c3b7604cd6
zhs007 <sssxueren@gmail.com>
2017-11-21 11:49:09 +0800

    upd

--- bin/sina_finance.js
+++ bin/sina_finance.js
@@ -61,7 +61,7 @@ CrawlerMgr.singleton.init().then(() => {
 
         // await startJYMX2Crawler('sh600000', '2005-01-01', '2017-11-12', HEADLESSCHROME_NAME);
 
-        await startAllStockPriceDayCrawler('2002-01-01', '2003-12-31');
+        await startAllStockPriceDayCrawler('2000-01-01', '2001-12-31');
 
         CrawlerMgr.singleton.start(true, true, async () => {
             // await StockMgr.singleton.saveStockBase();
